dfd399208feb4edada98ac6b52c72c79e488452b
Franco Peratta <franco.peratta@hotmail.com>
2023-03-29 21:32:21 -0300

    Create AdminController

---
+++ src/controllers/admin.ts
@@ -0,0 +1,84 @@
+import { prisma } from "../config/db"
+import { Request, Response } from "express"
+
+export const getAll = async (req: Request, res: Response) => {
+	try {
+		const data = await prisma.admin.findMany()
+		res.json({ data })
+	} catch (error) {
+		res.json({ msg: "Error, couldn't retrieve admins", error })
+		console.log(error)
+	}
+}
+
+export const getAdminById = async (req: Request, res: Response) => {
+	const adminId = parseInt(req.params.id)
+	try {
+		const data = await prisma.admin.findUnique({
+			where: {
+				id: adminId
+			}
+		})
+
+		res.json({ msg: "Admin retrieved SUCCESSFULLY", data })
+	} catch (error) {
+		res.json({ msg: "Error, couldn't retrieve admin", error })
+		console.log(error)
+	}
+}
+
+export const addAdmin = async (req: Request, res: Response) => {
+	const admin = req.body
+	try {
+		const newAdmin = await prisma.admin.create({
+			data: {
+				name: admin.name,
+				user: {
+					create: {
+						email: admin.email,
+						password: "password",
+						role: "admin"
+					}
+				}
+			}
+		})
+		res.json({ msg: "Admin added SUCCESSFULLY", data: newAdmin.id })
+	} catch (error) {
+		res.json({ msg: "Error, couldn't add the admin ", error })
+		console.log(error)
+	}
+}
+
+export const updateAdmin = async (req: Request, res: Response) => {
+	const adminId = parseInt(req.params.id)
+	const updatedAdmin = req.body
+	try {
+		const data = await prisma.admin.update({
+			where: {
+				id: adminId
+			},
+			data: {
+				name: updatedAdmin.name
+			}
+		})
+		res.json({ msg: "Admin updated SUCCESSFULLY", data })
+	} catch (error) {
+		res.json({ msg: "Error, couldn't update admin", error })
+		console.log(error)
+	}
+}
+
+export const deleteAdmin = async (req: Request, res: Response) => {
+	const adminId = parseInt(req.params.id)
+	try {
+		const admin = await prisma.admin.delete({
+			where: {
+				id: adminId
+			}
+		})
+		res.json({ msg: "Admin deleted SUCCESSFULLY", data: admin.id })
+	} catch (error) {
+		res.json({ msg: "Error, couldn't delete admin", error })
+		console.log(error)
+	}
+}
